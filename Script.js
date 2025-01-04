13842a06bfc4ac93740e7ac5e33d1319c18a3e19
khadka <khadkarachandra981@gmil.com>
2025-01-05 01:48:26 +0545

    test folder

--- Script.js
+++
@@ -1,6 +0,0 @@
-  const menu = document.querySelector("#menu");
-  const nav = document.querySelector(".links");
-  menu.onclick = () => {
-    menu.classList.toggle("bx-x");
-    nav.classList.toggle("active");
-  };

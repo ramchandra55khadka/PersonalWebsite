71caa6c922766b21030a089ca5ec9f683a8ab952
khadka <khadkarachandra981@gmil.com>
2025-01-05 00:42:44 +0545

    commit forth

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
-const menu = document.querySelector("#menu");
-const nav = document.querySelector(".links");
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  nav.classList.toggle("active");
-};
+  const menu = document.querySelector("#menu");
+  const nav = document.querySelector(".links");
+  menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    nav.classList.toggle("active");
+  };
